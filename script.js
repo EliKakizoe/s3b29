0eb97204ac84e6aee3a317849431a346f1e2d95c
Eli Kakizoe <81199514+EliKakizoe@users.noreply.github.com>
2022-03-10 16:54:59 -0400

    final

---
+++ script.js
@@ -0,0 +1,20 @@
+//document.getElementById("");
+//document.createElement("");
+//document.createTextNode();
+//Node.appendChild();
+
+
+const elemente0 = document.createElement("p");
+elemente0.innerText = "hello world";
+
+
+
+const elemente1 = document.createElement("p");
+elemente1.innerText = "hello world"
+
+const elemente2 = document.createTextNode("hello world")
+
+
+document.body.appendChild(elemente0);
+document.body.appendChild(elemente1);
+document.body.appendChild(elemente2);
